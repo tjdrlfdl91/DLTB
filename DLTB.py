09b45af109625ed70123df3320b36b851c8aa9c2
Seul Gi Han(한슬기) <mogae@bluehole.net>
2018-10-28 19:35:29 +0900

    Let's get started

---
+++ DLTB.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 28 18:49:41 2018
+
+@author: mogae
+"""
+
+
+cwd = "C:/Users/mogae/Documents/Python_Scripts/DeepLearningTutorial/DLTB"
+
+import os
+os.getcwd()
+os.chdir(cwd)
+
+
+
+
+
